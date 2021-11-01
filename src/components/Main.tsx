fa0622470820da67e21f95b0a14c83964792b8d1
Евгения Никонова <beagle-elgaeb@ya.ru>
2021-11-01 13:31:13 +0300

    Добавлены стили

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,8 @@
 import { ChangeEvent } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from 'react-router-dom';
-import { Action, ReduxState } from "../redux/types"
+import { useHistory } from "react-router-dom";
+import { Action, ReduxState } from "../redux/types";
+import styled from "styled-components/macro";
 
 function Main() {
   const history = useHistory();
@@ -11,21 +12,93 @@ function Main() {
   const password = useSelector((state: ReduxState) => state.password);
 
   const loginInput = (evt: ChangeEvent<HTMLInputElement>) => {
-    dispatch<Action>({ type: "INPUT", data: { name: evt.target.name as "login" | "password", value: evt.target.value } });
+    dispatch<Action>({
+      type: "INPUT",
+      data: { name: evt.target.name as "login" | "password", value: evt.target.value },
+    });
   };
 
   const logIn = () => {
     dispatch({ type: "AUTH" });
-    history.push('/profile');
+    history.push("/profile");
   };
 
   return (
-    <div>
-      <input onChange={loginInput} value={login || ""} name="login" type="text" placeholder="Login" />
-      <input onChange={loginInput} value={password || ""} name="password" type="text" placeholder="Password" />
-      <button type="button" onClick={logIn} disabled={!(login === "developer21" && password === "123456")}>LogIn</button>
-    </div>
+    <MainContainer>
+      <Input
+        onChange={loginInput}
+        value={login || ""}
+        name="login"
+        type="text"
+        placeholder="Login"
+      />
+      <Input
+        onChange={loginInput}
+        value={password || ""}
+        name="password"
+        type="text"
+        placeholder="Password"
+      />
+      <Button
+        type="button"
+        onClick={logIn}
+        disabled={!(login === "developer21" && password === "123456")}
+      >
+        LogIn
+      </Button>
+    </MainContainer>
   );
 }
 
 export default Main;
+
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+`;
+
+const Input = styled.input`
+  height: 33px;
+  width: 150px;
+  box-sizing: border-box;
+  background: transparent;
+  border: 1px solid Green;
+  border-radius: 6px;
+  outline: none;
+  font-size: 20px;
+  line-height: 22px;
+  color: Green;
+  cursor: pointer;
+  margin: 0 10px 0 0;
+
+  ::placeholder {
+    color: LightGreen;
+  }
+
+  :hover {
+    box-shadow: 1px 1px 5px Green;
+  }
+`;
+
+const Button = styled.button`
+  height: 33px;
+  width: 100px;
+  box-sizing: border-box;
+  background: ${({ disabled }) => (disabled ? "LightGray" : "LightGreen")};
+  border: none;
+  border-radius: 6px;
+  outline: none;
+  font-size: 20px;
+  line-height: 22px;
+  color: ${({ disabled }) => (disabled ? "White" : "Green")};
+  cursor: pointer;
+  margin: 0;
+
+  :hover {
+    box-shadow: 1px 1px 5px Green;
+  }
+`;
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,31 @@
-import { useSelector } from 'react-redux';
-import { ReduxState } from '../redux/types';
+import { useSelector } from "react-redux";
+import styled from "styled-components/macro";
+import { ReduxState } from "../redux/types";
 
 function Profile() {
   const login = useSelector((state: ReduxState) => state.login);
   const isLogin = useSelector((state: ReduxState) => state.isLogin);
 
   return (
-    <div>
-      <h1>{isLogin ? `${login}` : "Необходимо авторизоваться"}</h1>
-    </div>
+    <ProfileContainer>
+      <Title>{isLogin ? `${login}` : "Необходимо авторизоваться"}</Title>
+    </ProfileContainer>
   );
 }
 
 export default Profile;
+
+const ProfileContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+`;
+
+const Title = styled.h1`
+  font-size: 50px;
+  line-height: 52px;
+  color: MediumVioletRed;
+`;
